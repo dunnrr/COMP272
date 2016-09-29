60322435ffe6de18e175a4b425dedcdefb978c8a
Ryan <ryan@localhost.localdomain>
2016-09-28 23:47:05 -0700

    Question 5 ready

--- Assignment3/Graph.h
+++ Assignment3/Graph.h
@@ -1,4 +1,17 @@
+/******************************************************************************
+* Implementation of a specific graph for Quesion 5 of Assignment 3 in COMP 272
+*
+* Author:	Robert Ryan Dunn
+* Stu. No.:	3070207
+* Date:		28 August, 2016
+******************************************************************************/
 
+
+#ifndef _GRAPH_H_
+#define _GRAPH_H_
+
+
+#include "RuntimeException.h"
 #include <vector>
 #include <string>
 #include <fstream>
@@ -17,98 +30,64 @@ const int SIZE = 16;
 
 class Graph
 {
-
 public:
-	void DFS(char x);
-	void printMatrix();
-	void printPath();
-	void initialize(string filename);
-	int matrix [SIZE][SIZE];
-	char map[SIZE];
-private:
-	bool visited[SIZE] = {false};
-	void visit(int node);
-	int findUnused(int node);
-//	int findUnusedEdges(int node);
-	vector<char> path;
-//	stack<char> S;
+	Graph();										//constructor
+	void findPath(char x);							//find path
+	void printMatrix();								//print matrix
+	void printPath();								//print path
+protected:
+	void addEdge(char i, char j);					//add edge
+	int matrix [SIZE][SIZE] = {0};					//adjacency matrix
+	char map[SIZE];									//array of nodes
+	bool visited[SIZE] = {false};					//array of visited nodes
+	void visit(int node);							//visit node
+	int findUnused(int node);						//find an unused path
+	vector<char> path;								//path
 };
-/*
-int Graph::findUnusedEdges(int node)
-{
-	for (int i = 0; i < SIZE; i++)
-		if (matrix[node][i] == 1)
-}
-*/
-int Graph::findUnused(int node)
-{
-	for (int i = 0; i < SIZE; i++)
-		if (matrix[node][i] == 1 && matrix[i][node] == 1)
-			return i;
-	return -1;
-}
 
-void Graph::visit(int node)
-{
-	if (visited[node])
-	{
-		path.push_back(map[node]);
-		return;
-	}
-	visited[node] = true;
-//	S.push(node);
-	path.push_back(map[node]);
-	int edge = findUnused(node);
-	while(edge != -1)
-	{
-		matrix[node][edge] = 2;
-		visit(edge);
-		matrix[edge][node] = 2;
-		path.push_back(map[node]);
-		edge = findUnused(node);
-	}
+Graph::Graph() 										//constructor
+{ 
+	for (int i = 0; i < SIZE; i++)					//initialize map
+		map[i] = 'a' + i;
+													
+	addEdge('a','b');								//add edges
+	addEdge('a','e');
+	addEdge('a','f');
+	addEdge('b','c');
+	addEdge('c','d');
+	addEdge('c','f');
+	addEdge('d','g');
+	addEdge('e','i');
+	addEdge('f','j');
+	addEdge('g','h');
+	addEdge('g','j');
+	addEdge('g','k');
+	addEdge('h','o');
+	addEdge('i','j');
+	addEdge('i','m');
+	addEdge('i','n');
+	addEdge('k','o');
+	addEdge('l','p');
+	addEdge('n','o');
+	addEdge('o','p');
+}					
 
-	
-		
+void Graph::findPath(char x)						//find path
+{
+	int node = int(x - 'a');						//convert character to int
+	if (node < 0 || node >= SIZE)					//is index out of bounds?
+		throw IndexOutOfBounds(
+			"Index given is out of bounds.");		//throw error
+	visit(node);									//visit first node
 }
 
-void Graph::DFS(char x)
+void Graph::printMatrix()							//print matrix
 {
-	int node = int(x - 'a');
-	visit(node);
-}
-
-
-void Graph::initialize(string filename)
-{
-	std::ifstream infile(filename);
-	int temp;
-	char mapChar = 'a';
-	string line;
-	
-	int i = 0;
-	while(std::getline(infile, line))
-	{
-		std::stringstream ss(line);
-		int j = 0;
-		while( ss >> temp )
-		{
-			matrix[i][j] = temp;
-			j++;
-		}
-		map[i] = mapChar;
-		i++;
-		mapChar++;
-	}
-}
-
-void Graph::printMatrix()
-{
-	cout << endl << endl << "  ";
-	for (int i = 0; i < SIZE; i++)
+	cout << endl << "Matrix: " <<  endl << "  ";	
+	for (int i = 0; i < SIZE; i++)					//show map columns
 		cout << map[i] << " ";
 	cout << endl;
-	for(int i = 0; i < SIZE; i++)
+	for(int i = 0; i < SIZE; i++)					//show map rows
 	{
 		cout << map[i] << " ";
 		for(int j = 0; j < SIZE; j++)
@@ -118,16 +97,60 @@ void Graph::printMatrix()
 	cout << endl;
 }
 
-void Graph::printPath()
+void Graph::printPath()								//print path
 {
-	cout << "{";
+	cout << "Path: {";
 	int i = 1;
-	for (char n : path)
+	for (char n : path)								//for all items in path
 	{
 		cout << n;
 		if (i < path.size())
 			cout << ", ";
 		i++;
 	}
-	cout << "}";
+	cout << "}" << endl << endl;
 }
+
+void Graph::addEdge(char i, char j)					//add edge
+{
+	int n = i - 'a';								//convert to integer
+	int m = j - 'a';								//convert to integer
+	if ( (n < 0 || n >= SIZE) || 
+		(m < 0 || m >= SIZE))						//is index out of bounds?
+		throw IndexOutOfBounds(
+			"Index given is out of bounds.");		//throw error
+	matrix[n][m] = 1;								//assign edge
+	matrix[m][n] = 1;								
+}
+
+void Graph::visit(int node)							//visit node
+{
+	path.push_back(map[node]);						//add node to path
+	if (visited[node])								//check if visited
+	{
+		return;										//return
+	}
+	visited[node] = true;							//set node to visited
+	int edge = findUnused(node);					//find unused path
+	while(edge != -1)								//while unused paths
+	{
+		matrix[node][edge] = 2;						//mark edge as used one way
+		visit(edge);								//visit node
+		matrix[edge][node] = 2;						//mark edge as used two way
+		path.push_back(map[node]);					//add node to path
+		edge = findUnused(node);					//find unused edge
+	}
+}
+
+int Graph::findUnused(int node)						//find an unused path
+{
+	for (int i = 0; i < SIZE; i++)					//for all possible paths
+		if (matrix[node][i] == 1 && 
+			matrix[i][node] == 1)					//is path unused?
+			return i;								//return path
+	return -1;										//return path not found
+}
+
+#endif
+
+
--- Assignment3/Q5.cpp
+++ Assignment3/Q5.cpp
@@ -1,13 +1,50 @@
+/******************************************************************************
+* Test program for the Implementation of findPath function
+* as required for COMP 272 assignment 3 question 5.
+*
+* Author: Robert Ryan Dunn
+* SN: 3070207
+* Date: 28 September, 2016
+******************************************************************************/
+
 #include "Graph.h"
 
-int main(void)
+void findPath(Graph& G, char start)					//finds the path
 {
-	Graph G;
-	G.initialize("AdjacencyMatrix.txt");
-	G.printMatrix();
-	G.printPath();
-	G.DFS('a');
-	G.printPath();
-	G.printMatrix();
-	return 0;
+	try
+	{
+		G.findPath(start);							//try to find path
+	}
+	catch (IndexOutOfBounds& err)					//show error 
+	{
+		cout << endl << "Exception: " << 
+			err.getMessage() << endl << endl;						
+	}
+}
+
+void testCase(int &test)							//prints test case value
+{
+	cout << "Test Case: " << test
+		<< endl;
+	test++;
+}
+
+int main(void)									
+{
+	Graph G;										//create graph
+	int test = 1;									//assign test case value
+	
+	testCase(test);									//print out test case no.
+	G.printMatrix();								//print matrix
+	G.printPath();									//print path
+	
+	testCase(test);									//print out test case no.
+	findPath(G,'q');								//find path (cause error)
+	
+	testCase(test);									//print out test case no.	
+	findPath(G,'a');								//find path
+	G.printMatrix();								//print matrix
+	G.printPath();									//print path
+
+	return EXIT_SUCCESS;
 }
